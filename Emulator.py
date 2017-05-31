80b4ada41debae598ab169577b884c45233499db
Juts <Jutslegameur@gmail.com>
2017-05-31 16:49:29 +0200

    Update Emulator.py

--- Emulator.py
+++ Emulator.py
@@ -1,6 +1,24 @@
-RAM = [0 for i in range(4096)]  # RAM
-V = [0 for i in range(16)]      # registers
+import Display
+import pygame, sys
+from pygame.locals import *
+
+display = Display.Window()
+
+RAM = [0]*4096                  # RAM
+V = [0]*16                      # registers
 I = 0                           # special register
 PC = 0                          # PC = Program Counter | addresse de l'instruction à executer
 SP = 0                          # SP = Stack Pointer | redirige vers le plus haut niveau du stack
-Stack = [0 for i in range(16)]  # Stack, 16 valeurs de 16 bits => max 65532 valeurs
+Stack = [0]*16                  # Stack, 16 valeurs de 16 bits => max 65532 valeurs
+
+def cls():
+    display.clear()
+
+progress = True
+
+while progress:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            progress = False
+
+pygame.quit()
